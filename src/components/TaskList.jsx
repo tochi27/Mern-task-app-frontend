afdae2b3d47b6521fa0fd5b4bdc392a2e1302758
tochi27 <tochukwuamaechina2703@gmail.com>
2024-02-29 10:41:19 +0100

    Pushing frontend to a separate repo

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,171 @@
+import React, { useEffect, useState } from "react";
+import TaskForm from "./TaskForm";
+import Task from "./Task";
+import { toast } from "react-toastify";
+import axios from "axios";
+import { URL } from "../App";
+import Hourglass from "../assets/Hourglass.gif";
+
+// http://localhost:5000/api/tasks
+
+const TaskList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [completedTasks, setCompletedTasks] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+  const [taskID, setTaskID] = useState("");
+
+  const [formData, setFormData] = useState({
+    name: "",
+    completed: false,
+  });
+  const { name } = formData;
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const getTasks = async () => {
+    setIsLoading(true);
+    try {
+      const { data } = await axios.get(`${URL}/api/tasks`);
+      setTasks(data);
+      setIsLoading(false);
+    } catch (error) {
+      toast.error(error.message);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getTasks();
+  }, []);
+
+  // To create a task
+  const createTask = async (e) => {
+    e.preventDefault();
+    if (name === "") {
+      return toast.error("Input field cannot be empty");
+    }
+    try {
+      await axios.post(`${URL}/api/tasks`, formData);
+      toast.success("Task added successfully");
+      // To clear input field after successfully adding a task
+      setFormData({ ...formData, name: "" });
+      // To reload the tasks
+      getTasks();
+    } catch (error) {
+      toast.error(error.message);
+      console.log(error);
+    }
+  };
+
+  // To delete a task
+  const deleteTask = async (id) => {
+    try {
+      await axios.delete(`${URL}/api/tasks/${id}`);
+      getTasks();
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  // To highlight a task for updating/deleting
+  const getSingleTask = async (task) => {
+    setFormData({ name: task.name, completed: false });
+    setTaskID(task._id);
+    setIsEditing(true);
+  };
+
+  // To update a task
+  const updateTask = async (e) => {
+    e.preventDefault();
+    if (name === "") {
+      return toast.error("Input field cannot be empty.");
+    }
+    try {
+      await axios.put(`${URL}/api/tasks/${taskID}`, formData);
+      // To clear input field after successfully updating a task
+      setFormData({ ...formData, name: "" });
+      // To cancle editing mode
+      setIsEditing(false);
+      // To reload the tasks
+      getTasks();
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  // To set a task to completed
+  const setToComplete = async (task) => {
+    const newFormData = {
+      name: task.name,
+      completed: true,
+    };
+    try {
+      await axios.put(`${URL}/api/tasks/${task._id}`, newFormData);
+      getTasks();
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    const cTask = tasks.filter((task) => {
+      return task.completed === true
+    })
+    setCompletedTasks(cTask)
+  }, [tasks])
+
+  return (
+    <div>
+      <h2>Task Manager</h2>
+      <TaskForm
+        name={name}
+        handleInputChange={handleInputChange}
+        createTask={createTask}
+        isEditing={isEditing}
+        updateTask={updateTask}
+      />
+      {tasks.length > 0 && (
+        <div className="--flex-between --pb">
+          <p>
+            <b>Total Tasks:</b> {tasks.length}
+          </p>
+          <p>
+            <b>Completed Tasks:</b> {completedTasks.length}
+          </p>
+        </div>
+      )}
+
+      <hr />
+
+      {isLoading && (
+        <div className="--flex-center">
+          <img src={Hourglass} alt="Loading" />
+        </div>
+      )}
+      {!isLoading && tasks.length === 0 ? (
+        <p className="--py">No Task added. Please add a Task</p>
+      ) : (
+        <>
+          {tasks.map((task, index) => {
+            return (
+              <Task
+                key={task._id}
+                task={task}
+                index={index}
+                deleteTask={deleteTask}
+                getSingleTask={getSingleTask}
+                setToComplete={setToComplete}
+              />
+            );
+          })}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default TaskList;
